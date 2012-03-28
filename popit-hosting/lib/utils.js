e02020c946e4f7d8ec91f510ac12cc715a945b1e
Edmund von der Burg <evdb@mysociety.org>
2012-03-28 14:34:51 +0100

    Add methods to crypt and compare passwords

--- popit-hosting/lib/utils.js
+++ popit-hosting/lib/utils.js
@@ -1,4 +1,5 @@
-var check = require('validator').check;
+var check      = require('validator').check,
+    bcrypt     = require('bcrypt');
 
 
 module.exports.is_email = function (val) {
@@ -9,3 +10,21 @@ module.exports.is_email = function (val) {
         return false;
     }
 };
+
+
+module.exports.password_hash = function (plaintext, cb) {
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) throw err;
+        bcrypt.hash(plaintext, salt, function(err, hash) {
+            if (err) throw err;
+            cb(hash);
+        });
+    });
+};
+
+module.exports.password_hash_compare = function (plaintext, hash, cb) {
+    bcrypt.compare( plaintext, hash, function (err, is_same) {
+        if (err) throw err;
+        cb(is_same);
+    });
+}
--- popit-hosting/tests/utils.js
+++ popit-hosting/tests/utils.js
@@ -1,5 +1,6 @@
 var utils = require('../lib/utils'),
-    _     = require('underscore');
+    _     = require('underscore'),
+    async = require('async');
 
 exports.test_is_email = function ( test ) {
 
@@ -30,3 +31,49 @@ exports.test_is_email = function ( test ) {
 };
 
 
+exports.test_password_crypting = function (test) {
+
+    test.expect( 5 );
+    var plaintext = 'secr3t';
+    var hashed    = null;
+
+    async.series([
+        function (cb) {
+            // hash the password, check we get something
+            utils.password_hash(
+                plaintext,
+                function (h) { 
+                    hashed = h;
+                    test.notEqual( null     , hashed, "have a password: " + hashed );
+                    test.notEqual( plaintext, hashed, "password changed when hashed" );
+                    cb(null);
+                }
+            );
+        },
+        function (cb) {
+            // generate a new hash, check it is different
+            utils.password_hash( plaintext, function (new_hash) {
+                test.notEqual( hashed, new_hash, "new salt used" );
+                cb(null);
+            });
+        },
+        function (cb) {
+            // check that the correct password compares correctly
+            utils.password_hash_compare( plaintext, hashed, function (is_same) {
+                test.ok( is_same, "password and hash compare as expected" );
+                cb(null);
+            });             
+        },
+        function (cb) {
+            // check that a wrong password does not match 
+            utils.password_hash_compare( 'bogus', hashed, function (is_same) {
+                test.ok( !is_same, "bad password and hash compare as expected" );
+                cb(null);
+            });             
+        },
+    ], function (err) {
+        if (err) throw err;
+        test.done()
+    });
+    
+    };
