a3848d4e183e745657aea416f6c7a52fdbc34c81
Edmund von der Burg <evdb@mysociety.org>
2012-03-28 13:22:16 +0100

    Create testing suite, start with some really basic tests

---
+++ popit-hosting/tests/utils.js
@@ -0,0 +1,31 @@
+var utils = require('../lib/utils'),
+    _     = require('underscore');
+
+exports.test_is_email = function ( test ) {
+
+    var good = [
+        'bob@example.com',
+    ];
+    
+    var bad = [
+        'foo',
+        'foo@',
+        '@example.com',
+        '',
+        null
+    ];
+
+    test.expect( good.length + bad.length );
+
+    _.each( good, function ( email ) {
+        test.ok( utils.is_email(email), "is an email: " + email );        
+    });
+
+    _.each( bad, function ( email ) {
+        test.ok( ! utils.is_email(email), "not an email: " + email );        
+    });
+
+    test.done();
+};
+
+
