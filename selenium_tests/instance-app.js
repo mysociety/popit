1fe06c6900822a8ddd1ae075a3b6c4755ba99387
Edmund von der Burg <evdb@mysociety.org>
2012-04-11 20:12:30 +0100

    Test that instance is loaded correctly

--- selenium_tests/instance-app.js
+++ selenium_tests/instance-app.js
@@ -4,7 +4,8 @@ process.env.NODE_ENV = 'testing';
 var utils               = require('../lib/utils'),
     selenium_helpers    = require('../lib/testing/selenium'),
     config              = require('config'),
-    async               = require('async');
+    async               = require('async'),
+    $                   = require('jquery');
 
 module.exports = {
 
@@ -25,7 +26,7 @@ module.exports = {
     },
     
     "Check that non existent site gives correct error": function (test) {
-
+    
         var browser = selenium_helpers.new_instance_browser( 'does-not-exist' );
         
         test.expect(2);
@@ -33,10 +34,37 @@ module.exports = {
         browser
             // on home page
             .assertTextPresent('Site not found')
-
+    
             // on any arbitrary page
             .open('/foo/bar/baz/bundy')
             .assertTextPresent('Site not found')
+    
+            // all done
+            .testComplete()
+            .end(function (err) {
+                test.ifError(err);
+                test.ok(true, "end of tests");
+                test.done();
+            });        
+    },
+    
+    
+    "Check that existing site gives correct error": function (test) {
+
+        var browser = selenium_helpers.new_instance_browser( 'foobar' );
+        
+        test.expect(3);
+        
+        browser
+            .assertTextPresent('Welcome to PopIt')
+
+            .getHtmlSource(function (html) {
+                test.equal(
+                    $(html).find("#popit_instance_name").text(),
+                    'foobar',
+                    "correct instance loaded"
+                );
+            })
 
             // all done
             .testComplete()
