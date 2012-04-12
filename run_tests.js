e1a926f006600784a80d25399f8ed9e196cc9e9b
Edmund von der Burg <evdb@mysociety.org>
2012-04-12 10:12:51 +0100

    Put all test files in one location

--- run_tests.js
+++ run_tests.js
@@ -5,12 +5,5 @@ process.env.NODE_ENV = 'testing';
 
 var reporter = require('nodeunit').reporters.verbose;
 
-reporter.run([
-    // normal unit tests
-    'tests',
-
-    // FIXME add smarts here to check that selenium and a local node are running,
-    // and offer help if they are not.
-    'selenium_tests',
-]);
+reporter.run([ 'tests' ]);
 
