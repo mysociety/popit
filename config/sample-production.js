5fcdccb3f2121afd84028bb3b60dc7202f770648
Edmund von der Burg <evdb@mysociety.org>
2012-04-23 12:56:00 +0100

    Default production config to SMTP (more likely to work on a server)

--- config/sample-production.js
+++ config/sample-production.js
@@ -12,4 +12,10 @@ module.exports = {
         domain_suffix: 'popit.example.com',
         cookie_secret: 'something-really-secret',
     },
+    email: {
+        transport:         'SMTP',
+        // See https://github.com/andris9/Nodemailer for config - empty fine for localhost:25
+        transport_options: { },
+    },
+    
 };
