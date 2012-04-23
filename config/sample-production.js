93f3afeae93694aaacaa316645dcdb4d33adc360
Edmund von der Burg <evdb@mysociety.org>
2012-04-23 12:19:16 +0100

    Sample production.js, and ignore the real one

---
+++ config/sample-production.js
@@ -0,0 +1,15 @@
+/*  Configuration file
+ *
+ *
+ *
+ */
+
+module.exports = {
+    hosting_server: {
+        domain:     'popit.example.com',
+    },
+    instance_server: {
+        domain_suffix: 'popit.example.com',
+        cookie_secret: 'something-really-secret',
+    },
+};
