9de42b1595fab00398ce53710fd5a2b99b2144b1
Edmund von der Burg <evdb@mysociety.org>
2012-04-23 16:40:30 +0100

    Switch over to new design for instance server

--- instance-app/app.js
+++ instance-app/app.js
@@ -87,7 +87,7 @@ app.configure(function(){
   
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/../public'));
   
   // sessions and auth
   app.use( express.cookieParser( config.instance_server.cookie_secret ) );
