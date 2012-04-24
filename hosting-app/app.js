aad2448cd53d37ed9b84107cd7457ff99421fb58
Edmund von der Burg <evdb@mysociety.org>
2012-04-24 12:41:00 +0100

    Switch hosting app to use new design

--- hosting-app/app.js
+++ hosting-app/app.js
@@ -31,7 +31,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/../public'));
 });
 
 app.configure('development', function(){
