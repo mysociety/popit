7038a1f783383e28db45c06d80c68aa1b8a0faa2
Jedidiah Broadbent <hello@jedidiah.eu>
2012-04-27 09:14:21 +0100

    Temporary basic front-end validation

--- public/js/popit.js
+++ public/js/popit.js
@@ -11,5 +11,32 @@ $(document).ready(function() {
 	});
 	
 	
+	$('#slug').keypress(function(e) {
+		
+		/* FIXME EVDB check this is the best way to do this and also to match with serverside checks */
+		var check_allowed_letter = function(ltr){
+			if(
+				(ltr >= 97 && ltr <= 122) // a-z
+				||
+				(ltr >= 65 && ltr <= 90) // A-Z
+				||
+				(ltr >= 48 && ltr <= 57) // 0-9
+				||
+				(ltr === 45 || ltr === 95) // - _
+			){
+				return true;
+			} else{
+				return false;
+			}
+		}
+		// Return True/False to prevent or allow the key being pressed
+		// or add an error class to display the hint when a disallowed key is pressed
+		return check_allowed_letter(e.keyCode);
+	}).change(function(e) {
+		// Check pasted text
+		var changedText = $(this).val();
+		var cleanedText = changedText.replace(/[^a-z0-9_\-]/gi, '-').toLowerCase();
+		$(this).val(cleanedText);
+	});
 	
 });
