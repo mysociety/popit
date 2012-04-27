05002326a610da8e63b6518ba5cb82285150b8a0
Edmund von der Burg <evdb@mysociety.org>
2012-04-27 15:03:34 +0100

    Allow user to upload images

--- instance-app/routes/index.js
+++ instance-app/routes/index.js
@@ -7,14 +7,33 @@ var Validator     = require('validator').Validator,
 
 exports.route = function (app) {
 
-    app.get('/', function(req, res){
-        res.render( 'index' );
+  app.get('/', function(req, res){
+    res.render( 'index' );
+  });
+
+  app.get('/image/:image_spec', function (req,res) {
+  
+    var image_bits = req.param('image_spec').split('-'),
+        image_id   = image_bits[0],
+        image_size = image_bits[1];
+  
+    req.popit.model("Image").findById( image_id, function(err,image) {
+      if (err)    return next(err);
+      if (!image) return next(new Error404());
+  
+      // FIXME - handle other sizes too
+      if (image_size != 'original') return next(new Error404());
+    
+      res.header('Content-Type', image.mime_type);
+      res.sendfile( req.popit.files_dir( image.local_path_original ) );    
     });
-    // Throw a 404 error
-    app.all('/*', function(req, res, next) {
-      next(new Error404());
-    });
-        
+  });
+
+  // Throw a 404 error
+  app.all('/*', function(req, res, next) {
+    next(new Error404());
+  });
+
 };
 
 
