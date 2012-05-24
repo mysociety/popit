a38d4dbaf236b5f42e2689c3797361a3d40d46ce
Edmund von der Burg <evdb@mysociety.org>
2012-05-24 14:56:30 +0100

    Add backbone and underscore to the available js

--- public/js/main-instance.js
+++ public/js/main-instance.js
@@ -2,7 +2,7 @@ require(
   {    
 
     /**
-     * NOTE - this is duplicated in ap.build.js.
+     * NOTE - this is duplicated in app.build.js.
     **/
 
     baseUrl: '/js',
@@ -10,9 +10,13 @@ require(
     paths: {
       "jquery":           './libs/jquery-1.7.2.min',
       "jquery.mailcheck": './libs/jquery.mailcheck.1.0.3.min',
-      // underscore: 'libs/underscore/underscore-min', // https://github.com/amdjs
-      // backbone:   'libs/backbone/backbone-min', // https://github.com/amdjs
-    
+
+      // AMD versions of these libraries:
+      //   https://github.com/amdjs/backbone/blob/master/backbone.js
+      //   https://github.com/amdjs/underscore/blob/master/underscore.js
+      'underscore': './libs/underscore-1.3.3-amd',
+      'backbone':   './libs/backbone-0.9.2-amd',
+
       // Require.js plugins
       'text':  './libs/requirejs/text-1.0.8.min',
       'order': './libs/requirejs/order-1.0.5.min',
