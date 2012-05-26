34abaf870ff2335a5850dd52cd41030dc3130d22
Dominik Moritz <domoritz@gmail.com>
2012-05-26 23:37:16 +0200

    colorful server start

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ connect(
 )
 .listen(config.server.port);
 
+console.log( '\033[1m started at: \033[32m' + new Date() + '\033[0m' );
 console.log(
-  "PopIt hosting and instance apps started: http://%s:%s",
+  "\033[1m PopIt hosting and instance apps started: \033[36mhttp://%s:%s\033[0m",
   config.hosting_server.host, config.server.port
 );
