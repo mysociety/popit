d31b88f87a730f7d4487fe2ef6a3a537119a3256
Dominik Moritz <domoritz@gmail.com>
2012-05-29 21:47:52 +0200

    migrations schema for parsed data

--- tests/unit/migration.js
+++ tests/unit/migration.js
@@ -3,7 +3,8 @@
 process.env.NODE_ENV = 'testing';
 
 var utils    = require('../../lib/utils'),
-    PopIt    = require('../../lib/popit');
+    PopIt    = require('../../lib/popit'),
+    MigrationApp    = require('../../lib/apps/migration');
     
 module.exports = {
     
@@ -21,7 +22,7 @@ module.exports = {
     },
     
     "migration database model": function ( test ) {    
-        test.expect( 9 );
+        test.expect( 11 );
         
         var migration = new this.Migration();
         test.ok( migration, "got new migation" );
@@ -30,7 +31,8 @@ module.exports = {
         test.equal( migration.created, Date(), 'current date' );
         test.equal( migration.source.name, null, 'empty source name' );
         test.equal( migration.source.mime_type, null, 'empty source mime_type' );
-        test.equal( migration.source.parsed, null, 'empty parsed source' );
+        test.equal( migration.source.parsed.header.length, 0, 'empty parsed source header' );
+        test.equal( migration.source.parsed.data, null, 'empty parsed source data' );
 
         migration.source.name ="FooBar";
         test.equal( migration.source.name, "FooBar", 'source name changed' );
@@ -38,11 +40,28 @@ module.exports = {
         migration.source.mime_type ="text/csv";
         test.equal( migration.source.mime_type, "text/csv", 'source mime_type changed' );
 
+        var array = [ 'alpha', 'beta' ]
+        migration.source.parsed.header = array;
+        test.equal( migration.source.parsed.header.length, array.length, 'parsed source header changed' );
+
         var obj = {"0" : ["foo", "bar", "baz"]};
-        migration.source.parsed = obj;
-        test.equal( migration.source.parsed, obj, 'parsed source changed' );
-        test.notEqual( migration.source.parsed, {"1": ["foo"]}, 'parsed source changed' );
+        migration.source.parsed.data = obj;
+        test.deepEqual( migration.source.parsed.data, obj, 'parsed source data changed' );
+        test.notDeepEqual( migration.source.parsed.data, {"1": ["foo"]}, 'parsed source data changed' );
+
+        test.done();
+    },
+
+    "migration parse csv": function ( test ) {    
+        test.expect( 0 );
+
+        // FIXME throws an error
+        //var migration = new this.MigrationApp();
+        //test.ok( migration, "got new migation app" );
+
+        // test if method exists
 
         test.done();
     }
+
 };
