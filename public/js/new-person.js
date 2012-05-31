9d9793e5b53078853177f087ab72a6bb3584b0b8
Edmund von der Burg <evdb@mysociety.org>
2012-05-31 17:05:17 +0100

    Factor out new person view

--- public/js/new-person.js
+++ public/js/new-person.js
@@ -5,25 +5,13 @@
 require(
   [
     'jquery',
-    'backbone-forms',
-    'Backbone',
-    'underscore',
-    'utils/slugify',
-    'templates/person/new',
     'popit/models/person',
-    'popit/views/suggestions',
-    'popit/collections/suggestions'
+    'popit/views/person-new'
   ],
   function (
     $,
-    BackboneForms,
-    Backbone,
-    _,
-    slugify,
-    personTemplate,
     PersonModel,
-    SuggestionsView,
-    SuggestionsCollection
+    NewPersonView
   ) {
 
     // handle the API wrapping the responses in result(s): {...}
@@ -34,94 +22,6 @@ require(
           return data.results || data.result || data;
         }
       }
-    });
-
-    var NewPersonView = Backbone.View.extend({
-
-      initialize: function () {
-        this.form = new BackboneForms({ model: this.model });        
-        this.suggestions     = new SuggestionsCollection();
-        this.suggestionsView = new SuggestionsView({collection: this.suggestions});
-      },
-      
-      render: function () {
-
-        // render the template and form
-        var $content = $( personTemplate({}) );
-        var $form    = $( this.form.render().el );
-
-        // add the contents of the form to the template content
-        $content.find('form').prepend( $form.children() );
-        
-        // update our element
-        this.$el.html( $content );
-
-        // give the list to the Suggestions view for rendering
-        this.$('ul.suggestions').html( this.suggestionsView.render().el );
-
-        return this;
-      },
-      
-      events: {
-        'submit form ':             'submitForm',
-        'keyup input[name=name]':   'nameEdit',
-      },
-      
-      submitForm: function (e) {
-        e.preventDefault();
-
-        var self   = this;
-        var form   = self.form;        
-        var errors = form.commit();
-
-        if (_.isEmpty(errors)) {
-          this.model.save(
-            {},
-            {
-              success: function (model, response) {
-                document.location = response.meta.edit_url;
-              },
-              error: function (model, response) {
-                var errors = $.parseJSON( response.responseText ).errors || {};
-                _.each(errors, function(val, key) {
-                  form.fields[key] && form.fields[key].setError(val);
-                });         
-              }
-            }
-          );
-        }
-
-      },
-      
-      nameEdit: function (e) {
-        // When the name is being entered we should fill in the slug. This will
-        // let the user edit the slug, or see that it can't be generated from the
-        // name. Also means that we don't need to explain why that field is there.
-        var $name = this.$(':input[name=name]');
-        var $slug = this.$(':input[name=slug]');
-        $slug.val( slugify( $name.val() ) );
-                
-        // Try to load matching people from the server and display them in the
-        // 'possible matches' list.
-        var self = this;
-        
-        if ($name.val()) {
-          self.suggestions.fetch({
-            data: { name: $name.val() },
-            success: function () {
-              self.suggestionsView.render();
-            }
-          });          
-        } else {
-          self.suggestions.reset();
-        }
-        
-        
-
-        return true;
-      },
-
-
     });
 
 
