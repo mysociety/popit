0fcbb8506c16d59e74f0dde35048853ae7a3ff28
Edmund von der Burg <evdb@mysociety.org>
2012-05-31 16:40:47 +0100

    Change code layout so that the refactoring is easier to see

--- public/js/new-person.js
+++ public/js/new-person.js
@@ -2,8 +2,27 @@
 //  Launch a backbone powered entry box when someone clicks the new-person button
 // ------------------------
 
-require(   [ 'jquery', 'backbone-forms', 'Backbone', 'underscore', 'utils/slugify', 'templates/person/new', 'templates/person/compact_list' ],
-  function (  $,        BackboneForms,    Backbone,   _          ,  slugify,         personTemplate,         compactListTemplate            ) {
+require(
+  [
+    'jquery',
+    'backbone-forms',
+    'Backbone',
+    'underscore',
+    'utils/slugify',
+    'templates/person/new',
+    'templates/person/compact_list',
+    'popit/models/person'
+  ],
+  function (
+    $,
+    BackboneForms,
+    Backbone,
+    _,
+    slugify,
+    personTemplate,
+    compactListTemplate,
+    PersonModel
+  ) {
 
     // handle the API wrapping the responses in result(s): {...}
     $.ajaxSetup({
