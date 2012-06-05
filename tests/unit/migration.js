e4c5ec0bb914d795abbb01b63c9d17e203b944b6
Dominik Moritz <domoritz@gmail.com>
2012-06-05 20:48:45 +0200

    Tests for migration first

--- tests/unit/migration.js
+++ tests/unit/migration.js
@@ -67,11 +67,164 @@ module.exports = {
           test.ifError(err);
           test.ok( parsed, "migration tests" );
           test.deepEqual( parsed, realParsedCsv, "migration tests" );
-        });
 
-        // test if method exists
+          test.done();
+
+        });
+    },
+
+     "inversion": function ( test ) {    
+        test.expect( 3 );
+
+        var migration = new MigrationApp();
+        test.ok( migration, "got new migation app" );
+
+        test.ok( migration.invert, "migration tests" );
+
+        var mappings =  [[ 'firstname', 'name', 'First name' ],
+            [ 'middlename', 'name', 'Middle name' ],
+            [ 'lastname', 'name', 'Last name' ],
+            [ 'twitter', 'link', 'Twitter' ],
+            [ 'facebook', 'link', 'Facebook' ]];
+        var attributes = [ 'John',
+            'Peter',
+            'Doe',
+            '@funkyjohn',
+            'John Doe'];
+
+        var expect = {
+          'name': {
+            'First name': 'John', 
+            'Middle name': 'Peter', 
+            'Last name': 'Doe'},
+          'link': {
+            'Twitter': '@funkyjohn',
+            'Facebook': 'John Doe'
+          }};
+
+        var inverted = migration.invert(mappings, attributes);
+
+        test.deepEqual(inverted, expect, 'correctly inverted');
 
         test.done();
+    },
+
+    "migration import": function ( test ) {    
+        test.expect( 6 );
+
+        var migration = new MigrationApp();
+        test.ok( migration, "got new migation app" );
+
+        test.ok( migration.doImport, "migration tests" );
+
+        schema = 'person';
+        mappings = 
+            [ [ 'title', 'name', 'Title' ],
+              [ 'firstname', 'name', 'First name' ],
+              [ 'middlename', 'name', 'Middle name' ],
+              [ 'lastname', 'name', 'Last name' ],
+              [ 'name_suffix', 'name', 'Name suffix' ],
+              [ 'nickname', '', '' ],
+              [ 'party', 'organization', 'Party' ],
+              [ 'state', '', '' ],
+              [ 'district', '', '' ],
+              [ 'in_office', '', '' ],
+              [ 'gender', '', '' ],
+              [ 'phone', 'contact', 'Phone' ],
+              [ 'fax', 'contact', 'Fax' ],
+              [ 'website', 'link', 'Website' ],
+              [ 'webform', '', '' ],
+              [ 'congress_office', '', '' ],
+              [ 'bioguide_id', '', '' ],
+              [ 'votesmart_id', '', '' ],
+              [ 'fec_id', '', '' ],
+              [ 'govtrack_id', '', '' ],
+              [ 'crp_id', '', '' ],
+              [ 'twitter_id', 'id', 'Twitter' ],
+              [ 'congresspedia_url', '', '' ],
+              [ 'youtube_url', 'link', 'Youtube' ],
+              [ 'facebook_id', '', '' ],
+              [ 'official_rss', '', '' ],
+              [ 'senate_class', '', '' ],
+              [ 'birthdate', 'name', 'Birtdate' ] ];
+        data = {'675': 
+          [ 'Sen',
+            'John',
+            'A.',
+            'Doe',
+            '',
+            '',
+            'R',
+            'SD',
+            'Junior Seat',
+            '1',
+            'M',
+            '734-234-3545',
+            '734-234-3545',
+            'http://www.doe.senate.gov/',
+            'http://www.doe.senate.gov/public/index.cfm/contact',
+            'Office Building',
+            '56456',
+            '123',
+            'S2SD00068',
+            '400546',
+            'N00044342',
+            '@thedoe',
+            'http://www.opencongress.org/wiki/John_Doe',
+            'http://www.youtube.com/JohnDoe',
+            '63002536261',
+            '',
+            'III',
+            '02/04/1900' ],
+        '676': 
+          [ 'Sen',
+            'Peter',
+            'L.',
+            'Johnson',
+            '',
+            '',
+            'D',
+            'SD',
+            'Junior Seat',
+            '1',
+            'M',
+            '734-234-3547',
+            '734-234-3548',
+            'http://www.johnson.senate.gov/',
+            'http://www.johnson.senate.gov/public/index.cfm/contact',
+            'Office Building',
+            '6564566',
+            '3456',
+            '34566634',
+            '400546',
+            'N2356546',
+            '@mrsen',
+            'http://www.opencongress.org/wiki/Peter_Johnsson',
+            'http://www.youtube.com/PeterJohnsson',
+            '63002536261',
+            '',
+            'II',
+            '01/10/2010' ]};
+
+        var that = this;
+
+        // TODO test errors
+
+        migration.doImport(that.popit, schema, mappings, data, function(err, people){
+          test.ifError(err);
+          test.equal(people.length, 2, 'two people in people set');
+
+          var query = that.popit.model('Person').find().asc('name');
+
+          query.run(function(err, docs) {
+            test.ifError(err);
+
+            console.log(docs);
+            test.equal(docs.length, 2, 'two people in database');
+
+            test.done();
+          });
+        });
     }
 
 };
