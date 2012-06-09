bdb63f7c1d736ef9d75d33f04744d0a33aabecd8
Matthew Somerville <matthew@mysociety.org>
2012-06-09 01:06:07 +0100

    Add a noddy delete image function.

--- instance-app/routes/index.js
+++ instance-app/routes/index.js
@@ -1,9 +1,6 @@
-var Validator     = require('validator').Validator,
-    sanitize      = require('validator').sanitize,
-    PopIt         = require('../../lib/popit'),
-    utils         = require('../../lib/utils'),
-    mailer        = require('../../lib/mailer'),
-    Error404      = require('../../lib/errors').Error404;
+var Error404 = require('../../lib/errors').Error404,
+    requireUser = require('../../lib/middleware/route').requireUser,
+    image = require('../../lib/apps/image');
 
 exports.route = function (app) {
 
@@ -12,26 +9,8 @@ exports.route = function (app) {
     // res.render( 'index' );
   });
 
-  app.get('/image/:image_spec', function (req,res,next) {
-  
-    var image_bits = req.param('image_spec').split('-'),
-        image_id   = image_bits[0],
-        image_size = image_bits[1];
-  
-    // TODO - use an image 404 rather than an HTML one
-    if( ! utils.is_ObjectId(image_id) ) return next(new Error404());
-  
-    req.popit.model("Image").findOne( {_id: image_id}, function(err,image) {
-      if (err)    return next(err);
-      if (!image) return next(new Error404());
-  
-      // FIXME - handle other sizes too
-      if (image_size != 'original') return next(new Error404());
-    
-      res.header('Content-Type', image.mime_type);
-      res.sendfile( req.popit.files_dir( image.local_path_original ) );    
-    });
-  });
+  app.get('/image/:image_spec', image.get);
+  app.get('/image/:image_spec/delete', requireUser, image.delete);
 
   // Throw a 404 error
   app.all('/*', function(req, res, next) {
@@ -39,5 +18,4 @@ exports.route = function (app) {
   });
 
 };
-
 
