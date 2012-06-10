17302f1595424037a2db45b3ff8c418c0511495e
Chetan Bansal <chetan1@gmail.com>
2012-06-10 22:37:22 +0530

    Added route for About page

--- instance-app/app.js
+++ instance-app/app.js
@@ -143,6 +143,9 @@ app.configure(function(){
   var organisation_app_factory = require('../lib/apps/organisation');
   app.use('/organisation', organisation_app_factory() );
 
+  var about_app_factory = require('../lib/apps/about');
+  app.use('/about', about_app_factory() );
+  
   app.use(app.router);
   
   app.use( require('../lib/errors').errorHandler );
