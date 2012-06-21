1bcbd6d4d907eb956c6ebb1c14f6b4a8d6c21038
Edmund von der Burg <evdb@mysociety.org>
2012-06-21 19:29:20 +0100

    started putting common steps into the test case class for reuse

--- tests/browser_based/basics.rb
+++ tests/browser_based/basics.rb
@@ -1,11 +1,12 @@
 require 'popit_watir_test_case'
 
-class Basics < InstanceWatirTestCase
+class Basics < PopItWatirTestCase
 
-  def test_exit_code_correctly_produced
-    b = @b
-    b.goto '/'
-    assert_match( /PopIt/, b.text )
+  def test_site_can_be_deleted_and_fixture_loaded
+    goto_test_instance
+    load_test_fixtures
+    goto_home_page
+    assert_match( /PopIt/, @b.text )
   end
 
 end
--- tests/browser_based/popit_watir_test_case.rb
+++ tests/browser_based/popit_watir_test_case.rb
@@ -1,7 +1,8 @@
 require 'test/unit'
 require 'watir-webdriver'
+require 'uri'
 
-class InstanceWatirTestCase < Test::Unit::TestCase
+class PopItWatirTestCase < Test::Unit::TestCase
 
   def setup
 
@@ -10,18 +11,43 @@ class InstanceWatirTestCase < Test::Unit::TestCase
     # 3100. This should apply to the test suite when run from the Makefile
     # (which will also build the assets and start the testing server on that
     # port). 
-    @test_port         = ENV['NODE_ENV'] == 'testing' ? 3100 : 3000
-    @test_instance_url = "http://test.127-0-0-1.org.uk:#{@test_port}/"
+    @test_port = ENV['NODE_ENV'] == 'testing' ? 3100 : 3000
     @test_hosting_url  = "http://www.127-0-0-1.org.uk:#{@test_port}/"
 
     # create the browser and go to the homepage
     @b = Watir::Browser.new :chrome
-    @b.goto @test_instance_url
-
+    @b.goto @test_hosting_url
   end
 
   def teardown
     @b.quit
+  end
+
+  def goto relative_url
+    current_url = @b.url
+    next_url = URI.join current_url, relative_url
+    @b.goto next_url.to_s
+  end
+
+  def goto_dev_page
+    goto '/_dev'
+  end
+
+  def goto_home_page
+    goto '/'
+  end
+
+  def goto_test_instance
+    @b.goto @test_hosting_url
+    goto_dev_page
+    @b.input(:name, 'instance_slug').send_keys('test')
+    @b.button(:id, 'add_instance_to_master').click
+  end
+
+  def load_test_fixtures
+    goto_dev_page
+    @b.button(:id, 'delete_this_instance_database').click    
+    @b.button(:id, 'load_testing_fixture').click    
   end
 
 end
