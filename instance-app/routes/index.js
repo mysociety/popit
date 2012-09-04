92243d1ac227e9ff1159bc29b8249f1d6efeb60c
Edmund von der Burg <evdb@mysociety.org>
2012-09-04 14:18:32 +0100

    Put a list of people on the instance homepage

--- instance-app/routes/index.js
+++ instance-app/routes/index.js
@@ -1,13 +1,32 @@
-var Error404 = require('../../lib/errors').Error404;
+var Error404 = require('../../lib/errors').Error404,
+    async    = require('async');
 
 exports.route = function (app) {
 
   app.get('/search',  require('../../lib/apps/search').search );
 
-  app.get('/', function(req, res){
-    res.redirect( '/person' ); // until we have a more interesting homepage to show
-    // res.render( 'index' );
+  app.get('/', function(req, res, next){
+
+    async.parallel(
+      {
+        persons: function (callback) {
+          req.popit
+            .model('Person')
+            .find()
+            .limit(10)
+            .exec(callback);
+        },
+      },
+      function(err, results) {
+        if (err) return next(err);
+        res.locals(results);
+        res.render('index');
+      }
+    );
+
   });
+
+
 
   // Throw a 404 error
   app.all('/*', function(req, res, next) {
