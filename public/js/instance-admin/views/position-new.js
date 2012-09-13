9977b4ac8e8fc26fede7fd5a4e842b6ab709caa2
Edmund von der Burg <evdb@mysociety.org>
2012-09-13 10:20:01 +0100

    Refactor to extract reuseable code. Added non-alert error reporting

--- public/js/instance-admin/views/position-new.js
+++ public/js/instance-admin/views/position-new.js
@@ -25,6 +25,109 @@ define(
     submitFormHelper,
     SuggestionsView
   ) {
+    
+    function select2_create_arguments_for_model (args) {
+
+      // get the api url from the model
+      var ajax_url = new args.model().urlRoot;
+
+      return {
+        placeholder: args.placeholder,
+        minimumInputLength: 1,
+        allowClear: true,
+        ajax: {
+          url: ajax_url,
+          data: function (term, page) {
+            return {
+              name: term
+            };
+          },
+          results: function (data, page) {
+            var results = _.map(
+              data,
+              function (doc) {
+                return {
+                  id: doc._id,
+                  text: doc.name,
+                };
+              }
+            );
+            return { results: results };
+          }
+        },
+        createSearchChoice: function (term) {
+          return {
+            id: 0,
+            text: term,
+          };
+        },
+        initSelection: function (element, callback) {
+          var val = element.val();
+          if ( !val )
+            return callback(null);
+
+          var obj = new args.model({id: val});
+          obj.fetch({
+            success: function (model, response) {
+              callback({ id: response._id, text: response.name });
+            },
+            error: function (model, response) {
+              args.errors_list.append("<li>Could not fetch model from server</li>");
+              callback(null);
+            }
+          });
+         }
+      };
+    }
+    
+    function select2_create_arguments_for_autocompleter (args) {
+      return {
+        placeholder: args.placeholder,
+        minimumInputLength: 1,
+        ajax: {
+          url: args.autocomplete_url,
+          data: function (term, page) {
+            return {
+              term: term,
+            };
+          },
+          results: function (data, page) {
+            return {
+              results: _.map( data, function(i) { return {id: i, text: i}; } )
+            };
+          }
+        },
+        createSearchChoice: function (term) {
+          return {
+            id: term,
+            text: term
+          };
+        }
+      };
+    }
+    
+    function create_model_if_needed_for_select2_data (model, data) {
+      return function(deferred) {
+        if ( data === null ) {   // no doc selected
+          deferred.resolve(null);
+        } else if ( data.id  ) { // existing doc selected
+          deferred.resolve( data.id );
+        } else { // New doc needs to be created
+          new model({
+            name: data.text,
+          }).save({}, {
+            success: function (model, response) {
+              deferred.resolve( response._id );                
+            },
+            error: function (model, response) {
+              deferred.reject(response);
+            }
+          });
+        }
+      };
+    };
+    
+    
 
     var PositionNewView = Backbone.View.extend({
   
@@ -32,101 +135,41 @@ define(
       
       render: function () {
   
-        // render the template and form
+        // Render the template
         var $content = $( positionNewTemplate({}) );
   
-        this.$title_input = $content.find('[name=title]');
-        this.$title_input.select2({
-          placeholder: "Job Title",
-          minimumInputLength: 1,
-          ajax: {
-            url: "/autocomplete/position_title",
-            data: function (term, page) {
-              return {
-                term: term,
-              };
-            },
-            results: function (data, page) {
-              return {
-                results: _.map( data, function(i) { return {id: i, text: i}; } )
-              };
-            }
-          },
-          createSearchChoice: function (term) {
-            return {
-              id: term,
-              text: term
-            };
-          }
-        });
-
-        var select2_args = function (args) {
-          return {
-            placeholder: args.placeholder,
-            minimumInputLength: 1,
-            allowClear: true,
-            ajax: {
-              url: args.ajax_url,
-              data: function (term, page) {
-                return {
-                  name: term
-                };
-              },
-              results: function (data, page) {
-                var results = _.map(
-                  data,
-                  function (doc) {
-                    return {
-                      id: doc._id,
-                      text: doc.name,
-                    };
-                  }
-                );
-                return { results: results };
-              }
-            },
-            createSearchChoice: function (term) {
-              return {
-                id: 0,
-                text: term,
-              };
-            },
-            initSelection: function (element, callback) {
-              var val = element.val();
-              if ( !val )
-                return callback(null);
-
-              var obj = new args.model({id: val});
-              obj.fetch({
-                success: function (model, response) {
-                  callback({ id: response._id, text: response.name });
-                },
-                error: function (model, response) {
-                  alert('Could not fetch model from server');
-                  callback(null);
-                }
-              });
-             }
-          };
-        };
-        
-        this.$person_input = $content.find('[name=person]');
-        this.$person_input.val( this.model.get('person') );
-        this.$person_input.select2( select2_args({
-          placeholder: "Person's Name",
-          ajax_url:    "/api/v1/person",
-          model:       PersonModel
-        }) );
-          
+        // find the bits that are interesting and store them for easy access
+        this.$title_input        = $content.find('[name=title]');
+        this.$person_input       = $content.find('[name=person]');
         this.$organisation_input = $content.find('[name=organisation]');
+        this.$errors_list        = $content.find('ul.error');
+        
+        // If we have some details aready set store them
+        this.$title_input.val(        this.model.get('title') );
+        this.$person_input.val(       this.model.get('person') );
         this.$organisation_input.val( this.model.get('organisation') );
-        this.$organisation_input.select2( select2_args({
+        
+        // set up the title as an autocompletor
+        this.$title_input.select2(
+          select2_create_arguments_for_autocompleter({
+            placeholder:      "Job Title",
+            autocomplete_url: "/autocomplete/position_title"
+          })
+        );
+
+        // set up the model lookups
+        this.$person_input.select2( select2_create_arguments_for_model({
+          placeholder: "Person's Name",
+          model:       PersonModel,
+          errors_list: this.$errors_list
+        }) );
+        this.$organisation_input.select2( select2_create_arguments_for_model({
           placeholder: "Organisation's Name",
-          ajax_url:    "/api/v1/organisation",
-          model:       OrganisationModel
+          model:       OrganisationModel,
+          errors_list: this.$errors_list
         }) );          
   
-        // update our element
+        // add our content to the page
         this.$el.html( $content );
   
         return this;
@@ -141,66 +184,50 @@ define(
         
         var view = this;
 
+        // clear the errors
+        view.$errors_list.html('');
+
         // Get the title, error if it is not set
         var job_title_data = view.$title_input.select2('data');
         var job_title = job_title_data ? job_title_data.text : false;        
         if (!job_title) {
-          alert("Job Title required - FIXME don't use an alert" );
+          view.$errors_list.append("<li>Title is required</li>");
           return;
         }
-        
 
-        var create_model_if_needed = function (model, data) {
-          return function(deferred) {
-            if ( data === null ) {   // no doc selected
-              deferred.resolve(null);
-            } else if ( data.id  ) { // existing doc selected
-              deferred.resolve( data.id );
-            } else { // New doc needs to be created
-              new model({
-                name: data.text,
-              }).save({}, {
-                success: function (model, response) {
-                  deferred.resolve( response._id );                
-                },
-                error: function (model, response) {
-                  deferred.reject(response);
-                }
-              });
-            }
-          };
-        };
-
-
+        // for the person and organisation create the entry on the server if
+        // it is a new one (ie does not have an id)
         $.when(
           // get the person id, creating them if needed.
           $.Deferred(
-            create_model_if_needed(
+            create_model_if_needed_for_select2_data(
               PersonModel,
               view.$person_input.select2('data')
             )
           ),
           $.Deferred(
-            create_model_if_needed(
+            create_model_if_needed_for_select2_data(
               OrganisationModel,
               view.$organisation_input.select2('data')
             )
           )
         )
         .then(function(person_id, organisation_id) {
+
+          // create a new position
           var position = new PositionModel({
             person: person_id,
             organisation: organisation_id,
             title: job_title
           });
+
+          // Save it
           position.save({},{
             success: function (model, response ) {
-              console.log(response);
               document.location = response.meta.edit_url;              
             },
             error: function (model, response) {
-              alert('something went wrong');
-              console.log("ERROR:", response);
+              view.$errors_list.append('<li>Something went wrong saving the position to the server.</li>');
             }
           });
         });
