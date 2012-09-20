85ef8cc1f266627a2a89fec3dd144677c85cf1ec
Edmund von der Burg <evdb@mysociety.org>
2012-09-20 11:30:16 +0100

    Add in Watir tests to the test runner

--- tests/browser_based/run_tests.rb
+++ tests/browser_based/run_tests.rb
@@ -3,11 +3,16 @@ require 'test/unit'
 # make it possible to load the test cases from the local directory
 $:.unshift File.dirname(__FILE__)
 
+# FIXME - should not need to list all the tests here - should be able to extract them from the directory listing instead.
+
 require 'basics'
-require 'info_pages'
 require 'hosting'
+require 'info_pages'
 require 'instance'
 require 'instance_auth'
-require 'person_editing'
-require 'migration'
 require 'instance_info'
+require 'migration'
+require 'person_editing'
+require 'person_edit_list_details'
+require 'person_photo'
+require 'position_editing'
