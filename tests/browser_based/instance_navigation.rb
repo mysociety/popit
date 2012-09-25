24be8fff0f814d54c6f6291e666a41ce5ba5bf8a
Edmund von der Burg <evdb@mysociety.org>
2012-09-25 16:34:14 +0100

    Let users move back and forth between people and organisations

---
+++ tests/browser_based/instance_navigation.rb
@@ -0,0 +1,37 @@
+# encoding: UTF-8
+# coding: UTF-8
+# -*- coding: UTF-8 -*-
+
+require 'lib/popit_watir_test_case'
+require 'pry'
+require 'net/http'
+require 'uri'
+
+
+class NavigationTests < PopItWatirTestCase
+
+  def assert_path(expected)
+    assert_equal URI.parse(@b.url).path, expected
+  end
+  
+  def test_navigation_around_site
+    goto_instance 'test'
+    delete_instance_database
+    load_test_fixture
+
+    # home page
+    goto '/'
+
+    # go to org
+    @b.link(:text => 'United States Government').click
+    assert_path '/organisation/united-states-government'
+
+    # go to person and then back to the org
+    @b.link(:text => 'Bill Clinton').click
+    assert_path '/person/bill-clinton'
+    @b.link(:text => 'United States Government').click
+    assert_path '/organisation/united-states-government'
+    
+  end 
+
+end
