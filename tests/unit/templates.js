aa163f37b1556713672580d68f6c8bd23caa49bc
Edmund von der Burg <evdb@mysociety.org>
2012-10-30 20:02:23 +0000

    Clean up interface, make it a proper object

--- tests/unit/templates.js
+++ tests/unit/templates.js
@@ -5,44 +5,40 @@
 process.env.NODE_ENV = 'testing';
 
 var _         = require('underscore'),
-    templates = require('../../lib/templates');
+    Templates = require('../../lib/templates');
 
 module.exports = {
-    
-  "test loading basic templates" : function (test) {
-    test.expect(2);
-    templates.getTemplates({}, function(err, templates) {
-      test.ifError(err);
-      test.deepEqual(
-        _.keys(templates).sort(),
-        ['inner.html','outer.html']
-      );
-      test.done();
+
+  setUp: function (setUpDone) {
+
+    // Create the template wrapper
+    this.templates = new Templates({
+      templateDir: __dirname + '/../../lib/sample_templates'
     });
+
+    // init the templates, when done proceed
+    this.templates.loadFromDir(
+      __dirname + '/../../lib/sample_templates',
+      setUpDone
+    );
   },
-  
+    
   "test rendering a simple template" : function (test) {
-    test.expect(2);
-    templates.getTemplates({}, function(err, templates) {
-      test.ifError(err);
-      test.equal(
-        templates['inner.html'](),
-        "INNER_START\n\nINNER_END\n"
-      );
-      test.done();
-    });
+    test.expect(1);
+    test.equal(
+      this.templates.render('inner.html'),
+      "INNER_START\n\nINNER_END\n"
+    );
+    test.done();
   },
 
   "test rendering a template with an include" : function (test) {
-    test.expect(2);
-    templates.getTemplates({}, function(err, templates) {
-      test.ifError(err);
-      test.equal(
-        templates['outer.html'](),
-        "OUTER_START\n\nINNER_START\n\nINNER_END\n\n\nOUTER_END\n"
-      );
-      test.done();
-    });
+    test.expect(1);
+    test.equal(
+      this.templates.render('outer.html'),
+      "OUTER_START\n\nINNER_START\n\nINNER_END\n\n\nOUTER_END\n"
+    );
+    test.done();
   },
 
 };
