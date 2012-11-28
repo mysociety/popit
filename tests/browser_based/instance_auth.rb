452660ebc70e84dc6fab85dd044c6b4268dbbfdf
Edmund von der Burg <evdb@mysociety.org>
2012-11-28 16:23:12 +0000

    Merge the guest and auth tests

--- tests/browser_based/instance_auth.rb
+++ tests/browser_based/instance_auth.rb
@@ -82,5 +82,26 @@ class InstanceAuthTests < PopItWatirTestCase
     
 
   end
+  
+  def test_enabling_and_disabling_guest_access
+    goto_instance 'test'
+    delete_instance_database
+    load_test_fixture
 
+    # check that the instance starts with guest access turned off
+    goto '/'
+    assert_equal "Authorised edits only", @b.element(:id => 'instance_guest_status').text
+    
+    # enable guest access and check again
+    enable_guest_access
+    goto '/'
+    assert_equal "Guest edits allowed", @b.element(:id => 'instance_guest_status').text
+
+    # and off again
+    disable_guest_access
+    goto '/'
+    assert_equal "Authorised edits only", @b.element(:id => 'instance_guest_status').text
+
+  end 
+  
 end
