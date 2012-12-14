e3d9f589df4ca0da8782c11db30e2d72c71597da
Edmund von der Burg <evdb@mysociety.org>
2012-12-14 16:13:10 +0000

    Strip out code and tests for sub documents

--- tests/api/writing.js
+++ tests/api/writing.js
@@ -63,7 +63,7 @@ module.exports = {
           test.done();
         });
         
-      },
+    },
 
     "create a new person (bad values, expect error)": function (test) {
       var rest = this.rest;
@@ -182,9 +182,6 @@ module.exports = {
         create_error_test( 'put',  'person' ),
         create_error_test( 'del',  'person' ),
         create_error_test( 'post', 'person/4f9ea1316e8770d854c45a1e' ),
-        create_error_test( 'del',  'person/4f9ea1316e8770d854c45a1e/links' ),
-        create_error_test( 'put',  'person/4f9ea1316e8770d854c45a1e/links' ),
-        create_error_test( 'post', 'person/4f9ea1316e8770d854c45a1e/links/4f9ea1326e8770d854c45a26' ),
       ];
 
       test.expect( tests.length * 3 );
@@ -225,137 +222,6 @@ module.exports = {
           function(cb) {
             rest.del(url).on('complete', function(data, response) {
               test.equal(response.statusCode, 404, "got 404 - not found for subsequent delete");
-              cb();
-            });
-          },
-        ],
-        test.done
-      );
-    },
-
-    "check deleting embedded document": function (test) {
-      var rest = this.rest;
-      var embedded_url = 'person/4f9ea1316e8770d854c45a1e/links/4f9ea1326e8770d854c45a26';
-      var document_url = 'person/4f9ea1316e8770d854c45a1e';
-
-      test.expect(7);
-      
-      async.series(
-        [
-          // check that the document exists
-          function(cb) {
-            rest.get(document_url).on('complete', function(data, response) {
-              test.equal(response.statusCode, 200, "got 200 - parent document exists");              
-              cb();
-            });
-          },
-          // check that the embedded document exists
-          function(cb) {
-            rest.get(embedded_url).on('complete', function(data, response) {
-              test.equal(response.statusCode, 200, "got 200 - embedded document exists");              
-              cb();
-            });
-          },
-          // delete the document
-          function(cb) {
-            rest.del(embedded_url).on('complete', function(data, response) {
-              test.equal(response.statusCode, 204, "got 204 - embedded document deleted");
-              test.equal( data.length, 0, "no content returned" );      
-              cb();
-            });
-          },
-          // check that the document is now gone
-          function(cb) {
-            rest.get(embedded_url).on('complete', function(data,response) {
-              test.equal(response.statusCode, 404, "got 404 - embedded document gone");              
-              cb();
-            });
-          },
-          // delete the document again, should 404 as not found
-          function(cb) {
-            rest.del(embedded_url).on('complete', function(data, response) {
-              test.equal(response.statusCode, 404, "got 404 - not found for subsequent delete");
-              cb();
-            });
-          },
-          // check that the document has not been deleted
-          function(cb) {
-            rest.get(document_url).on('complete', function(data, response) {
-              test.equal(response.statusCode, 200, "got 200 - parent document exists");              
-              cb();
-            });
-          },
-        ],
-        test.done
-      );
-    },
-
-
-    "create embedded document": function (test) {
-      var rest = this.rest;
-      var document_url       = 'person/4f9ea1316e8770d854c45a1e';
-      var sub_collection_url = 'person/4f9ea1316e8770d854c45a1e/links';
-      var embedded_url       = 'person/4f9ea1316e8770d854c45a1e/links/4f9ea1326e8770d854c45a26';
-
-      // fill these in in the tests
-      var original_sub_collection_ids = [];
-      var sub_document_url = null;
-
-      test.expect(10);
-      
-      async.series(
-        [
-          // check that subcollection has some items in it, store the ids
-          function(cb) {
-            rest.get(sub_collection_url).on('complete', function(data, response) {
-              test.equal(response.statusCode, 200, "got 200 - sub_collection exists");              
-              
-              original_sub_collection_ids = _.pluck( data.results, '_id' );
-              test.ok( original_sub_collection_ids.length >= 1, "Got some existing content" );
-
-              cb();
-            });
-          },
-          // add a document to the collection
-          function (cb) {
-            rest
-              .post(sub_collection_url, { data: { url: 'http://test.com', comment: "sample url"}})
-              .on('complete', function (data, response) {
-                test.equal(response.statusCode, 200, "got 200 - embedded document created");              
-
-                // get the location of the new document
-                sub_document_url = response.headers.location;
-                test.ok(
-                  (new RegExp(sub_collection_url + '/[0-9a-f]{24}')).test(sub_document_url),
-                  "New entity in subcollection"
-                );
-
-                test.equal(data.ok, true, "got 'ok' in JSON" );
-                test.equal(data.api_url, sub_document_url, "got correct sub-document url" );
-                test.equal(data.result.url, 'http://test.com', "got object back too");
-
-                cb();
-              });
-          },
-          // check that the sub_document_url works
-          function (cb) {
-            rest.get(sub_document_url).on('complete', function(data, response) {
-              test.equal(data.result.url, 'http://test.com', "new link url correct");
-              cb();
-            });
-          },
-          // check that subcollection has more items in now
-          function(cb) {
-            rest.get(sub_collection_url).on('complete', function(data, response) {
-              test.equal(response.statusCode, 200, "got 200 - sub_collection exists");              
-              
-              var post_create_sub_collection_ids = _.pluck( data.results, '_id' );
-
-              test.ok(
-                original_sub_collection_ids.length == post_create_sub_collection_ids.length - 1,
-                "Got one more entry in collection"
-              );
-
               cb();
             });
           },
@@ -474,74 +340,6 @@ module.exports = {
           test.done();
         }
       );
-    },
-
-    "update a sub document": function (test) {
-      var rest         = this.rest;
-      var embedded_url       = 'person/4f9ea1316e8770d854c45a1e/links/4f9ea1326e8770d854c45a26';
-
-      test.expect(3);
-      
-      async.series(
-        [
-          // update one field
-          function (cb) {
-
-            // TODO: should test updating using the dot notation too
-
-            rest
-              .put(embedded_url, { data: { url: 'http://update.test/' } })
-              .on('complete', function(data, response) {
-                test.equal(response.statusCode, 200, "got 200");
-                cb();
-              });
-          },
-          // check it is changed (and other not)
-          function (cb) {
-            rest
-              .get(embedded_url)
-              .on('complete', function(data, response) {
-                test.deepEqual(
-                  data.result,
-                  {
-                    _id:     '4f9ea1326e8770d854c45a26',
-                    url:     'http://update.test/',
-                    comment: 'William J. Clinton Foundation',
-                  },
-                  "Embedded document updated as expected"
-                );
-                cb();
-              });
-          },
-        ],
-        function(err) {
-          test.ifError(err, 'No errors caught');
-          test.done();
-        }
-      );
-    },
-
-    "try to save a bad date to a position": function (test) {
-
-      // addresses https://github.com/mysociety/popit/issues/216
-
-      var dataJson = JSON.stringify({
-        "end_date": null,
-        "title": "Director"
-      });
-      
-      this.rest
-        .post(
-          'position/',
-          {
-            data: dataJson,
-            headers: {'Content-Type': 'application/json'},
-          }
-        )
-        .on('complete', function(data, response) {
-          test.equal(response.statusCode, 400, "got 400");
-          test.done();
-        });
     },
     
 };
