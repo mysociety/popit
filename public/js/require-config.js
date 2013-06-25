8d555fc55f27b98324fc3c3edf7662f16d7f2b64
Matthew Somerville <matthew@mysociety.org>
2013-06-25 17:30:57 +0100

    Update to Backbone 1.0.0.

--- public/js/require-config.js
+++ public/js/require-config.js
@@ -14,7 +14,7 @@ require.config({
     //   https://github.com/amdjs/backbone/blob/master/backbone.js
     //   https://github.com/amdjs/underscore/blob/master/underscore.js
     'underscore':       './libs/underscore-1.3.3-amd',
-    'Backbone':         './libs/backbone-0.9.2-amd',
+    'Backbone':         './libs/backbone-1.0.0-amd',
     'backbone-forms':   './libs/backbone-forms-0.9.0-amd',
 
     // Require.js plugins
