284901f5a6f5ba389e0313a8a01c6ed5ddb57901
Matthew Somerville <matthew@mysociety.org>
2013-07-05 14:24:15 +0100

    Give posts start/end dates.

--- public/js/instance-admin/models/post.js
+++ public/js/instance-admin/models/post.js
@@ -1,11 +1,19 @@
-define( [ 'Backbone' ], function ( Backbone  ) {
+define( [ 'Backbone', 'backbone-forms' ], function ( Backbone, BackboneForms ) {
   "use strict"; 
+
+  var NullText = BackboneForms.editors.Text.extend({
+    getValue: function() {
+      return this.$el.val() || null;
+    }
+  });
 
   return Backbone.Model.extend({
     urlRoot: '/api/v0.1/posts',
     schema: {
       label: { dataType: 'Text', validators: ['required'] },
       role: { dataType: 'Text' },
+      start_date: { title: 'Start date', type: NullText },
+      end_date: { title: 'End date', type: NullText },
       area: {
         type: 'Object', subSchema: {
           id: {},
