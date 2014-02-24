2a473f630c88d500e7f089fb9e9e25848a58604a
Chris Mytton <self@hecticjeff.net>
2014-02-24 10:31:29 +0000

    Update fixtures to use custom ids
    This means that we're testing the custom id functionality by default and
    also means there's no need to change all the url assertions in the
    tests.

--- fixtures/test_instance.js
+++ fixtures/test_instance.js
@@ -10,17 +10,16 @@ exports.users = [
 
 exports.persons = [
   {
-          "_id" : "4f9ea1306e8770d854c45a1d",
+          "_id" : "george-bush",
           "name" : "George Bush",
           "_internal": {
             "name_dm" : [ "JRJ", "KRK", "PX", "PX", "george", "bush" ],
             "name_words" : [ "george", "bush" ],
           },
-          "slug" : "george-bush",
           "summary" : "41th President of the United States"
   },
   {
-          "_id" : "4f9ea1316e8770d854c45a1e",
+          "_id" : "bill-clinton",
           "links" : [
               {
                   "url" : "http://www.clintonfoundation.org/",
@@ -32,17 +31,15 @@ exports.persons = [
             "name_dm" : [ "PL", "PL", "KLNT", "KLNT", "bill", "clinton" ],
             "name_words" : [ "bill", "clinton" ],
           },
-          "slug" : "bill-clinton",
           "summary" : "42nd President of the United States"
   },
   {
-          "_id" : "4f9ea1316e8770d854c45a1f",
+          "_id" : "george-w-bush",
           "name" : "George W. Bush",
           "_internal": {
             "name_dm" : [ "JRJ", "KRK", "", "", "PX", "PX", "george", "w.", "bush" ],
             "name_words" : [ "george", "w.", "bush" ],
           },
-          "slug" : "george-w-bush",
           "summary" : "43rd President of the United States",
           "images": [
             {
@@ -53,22 +50,20 @@ exports.persons = [
           ],
   },
   {
-          "_id" : "4f9ea1326e8770d854c45a20",
+          "_id" : "barack-obama",
           "name" : "Barack Obama",
           "_internal": {
             "name_dm" : [ "PRK", "PRK", "APM", "APM", "barack", "obama" ],
             "name_words" : [ "barack", "obama" ],
           },
-          "slug" : "barack-obama",
           "summary" : "44th President of the United States",
   }
 ];
 
 exports.organizations = [
   {
-    "_id": "4f9ea1326e8770d854c45a21",
+    "_id": "united-states-government",
     name: "United States Government",
-    slug: "united-states-government",
     summary: "The government of the United States of America is the federal government of the constitutional republic of fifty states that constitute the United States of America"
   },
 ];
@@ -92,26 +87,26 @@ exports.memberships = [
   {
     "_id":        "4f9ea1326e8770d854c45a22",
     role:         "President",
-    person_id:    "4f9ea1306e8770d854c45a1d", // george-bush
-    organization_id: "4f9ea1326e8770d854c45a21",
+    person_id:    "george-bush",
+    organization_id: "united-states-government",
   },
   {
     "_id":        "4f9ea1326e8770d854c45a23",
     role:         "President",
-    person_id:    "4f9ea1316e8770d854c45a1e", // bill-clinton
-    organization_id: "4f9ea1326e8770d854c45a21",
+    person_id:    "bill-clinton",
+    organization_id: "united-states-government",
   },
   {
     "_id":        "4f9ea1326e8770d854c45a24",
     role:         "President",
-    person_id:   "4f9ea1316e8770d854c45a1f", // george-w-bush
-    organization_id: "4f9ea1326e8770d854c45a21",
+    person_id:   "george-w-bush",
+    organization_id: "united-states-government",
   },
   {
     "_id":        "4f9ea1326e8770d854c45a25",
     role:         "President",
-    person_id:    "4f9ea1326e8770d854c45a20", // barack-obama
-    organization_id: "4f9ea1326e8770d854c45a21",
+    person_id:    "barack-obama",
+    organization_id: "united-states-government",
   },
 ];
 
