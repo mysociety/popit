67f0bd1717cfe51940a391ed21149bd0bf43623c
Chris Mytton <self@hecticjeff.net>
2014-06-16 14:48:05 +0100

    Autocomplete select2 for memberships

--- public/js/instance-admin/actions/membership-add-remove.js
+++ public/js/instance-admin/actions/membership-add-remove.js
@@ -6,25 +6,53 @@
 define([
   'jquery',
   'underscore',
+  'instance-admin/utils/select2-helpers',
   'text!templates/membership/new.html',
   'jquery.select2'
-], function($, _, membershipTemplate) {
+], function($, _, select2helpers, membershipTemplate) {
   "use strict";
+
+  function setupSelect2() {
+    var select2orgArgs = select2helpers.create_arguments_for_model({
+      placeholder: "e.g Apple Inc, UK Parliament, Kenyatta University",
+      url: '/autocomplete/organizations'
+    });
+
+    $('.js-membership-organization').each(function() {
+      var $input = $(this);
+      $input.select2(select2orgArgs);
+      $input.select2('data', {id: $input.val(), text: $input.data('org-name')});
+    });
+
+    var select2membershipArgs = select2helpers.create_arguments_for_autocompleter({
+      placeholder:      "e.g President, CEO, Professor, Coach",
+      autocomplete_url: "/autocomplete/memberships"
+    })
+
+    $('.js-membership-role').each(function() {
+      var $input = $(this);
+      $input.select2(select2membershipArgs);
+      $input.select2('data', {id: $input.val(), text: $input.val()});
+    });
+  }
 
   var template = _.template(membershipTemplate);
 
   $(function() {
-    $('.add-membership').click(function(e) {
+    $('#content').on('click', '.add-membership', function(e) {
       e.preventDefault();
       var newLi = $('<li/>').html(template({
-        i: $('ul.memberships li').length,
+        i: $('form.editing ul.memberships li').length,
         type: popit.type,
         member: {id: popit.data.id, name: popit.data.name},
         membership: {},
         organization: {}
       }));
-      $('ul.memberships').append(newLi);
+      $('form.editing ul.memberships').append(newLi);
+      setupSelect2();
     });
+
+    setupSelect2();
   });
 
   $('#content').on('click', '.delete-membership', function(e) {
