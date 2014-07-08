aff89585726fa484dfc669a283922defd2e3b89d
Chris Mytton <self@hecticjeff.net>
2014-07-08 09:30:21 +0100

    Split up the app from the server
    By separating these two modules we can test the application in isolation
    using supertest without having to worry about the server starting up and
    binding to a port.

--- server.js
+++ server.js
@@ -4,36 +4,11 @@
  * Module dependencies.
  */
 
-var express        = require('express'),
-    // connect        = require('connect'),
-    config         = require('config'),
-    winston        = require('./lib/popit_winston'),
-    hosting_app    = require('./hosting-app/app'),
-    utils          = require('./lib/utils'),
-    format         = require('util').format,
-    instance_app   = require('./instance-app/app');
-
-var app = express();
-
-// Intercept JSON content type GET requests and change the header.
-// this is a workaround for https://github.com/senchalabs/connect/issues/680 which hopefully will be fixed upstream
-app.use( function (req, res, next) {
-  if ( (req.method == "GET" || req.method == "DELETE" ) && req.headers['content-type'] == 'application/json' ) {
-    delete req.headers['content-type'];
-  }
-  next();
-});
-
-// match the hosting app host...
-app.use(
-  express.vhost(
-    config.hosting_server.host,
-    hosting_app
-  )
-);
-
-// ...or fall through to the instance app
-app.use(instance_app);
+var app = require('./app');
+var config = require('config');
+var winston = require('./lib/popit_winston');
+var format = require('util').format;
+var utils = require('./lib/utils');
 
 app.listen(config.server.port);
 
