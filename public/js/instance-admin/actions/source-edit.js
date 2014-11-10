087fd901d1fcd8504a33ee01aae76c3240baf0f2
Chris Mytton <self@hecticjeff.net>
2014-11-10 18:16:31 +0000

    Allow adding source urls to a person
    This adds an extra tab to the person pages which allows adding source
    urls to the record to indicate where the information for them comes
    from.

---
+++ public/js/instance-admin/actions/source-edit.js
@@ -0,0 +1,38 @@
+/*global popit:false */
+// Launch a backbone powered entry box when someone adds/edits a source
+
+define(
+  [
+    'jquery',
+    'underscore',
+    'text!templates/source/edit.html'
+  ],
+  function (
+    $,
+    _,
+    sourceTemplate
+  ) {
+    "use strict";
+
+    $(function(){
+      if (typeof popit === 'undefined') {
+         return;
+      }
+
+      var template = _.template(sourceTemplate);
+
+      $('#content').on('click', '.source-edit', function(e) {
+        e.preventDefault();
+
+        var newLi = $('<li/>').html(template({i: $('ul.sources li').length, source: ''}));
+        $('ul.sources').append(newLi);
+      });
+
+      $('.source-delete').click(function(e) {
+        e.preventDefault();
+        $(this).closest('li').remove();
+      });
+
+    });
+  }
+);
--- public/js/run-instance-admin.js
+++ public/js/run-instance-admin.js
@@ -21,6 +21,7 @@ require ([
   'instance-admin/actions/other_name-edit',
   'instance-admin/actions/identifier-edit',
   'instance-admin/actions/links-edit',
+  'instance-admin/actions/source-edit',
   'instance-admin/actions/person-edit',
   'instance-admin/actions/entity-new',
   'instance-admin/actions/organization-edit',
