30500f02f17b7e18595932030c8160a773498154
Chris Mytton <chrismytton@gmail.com>
2015-02-13 12:08:01 +0000

    Fix vagrant provisioning script
    - Don't run the script as root, use sudo instead when needed
    - Remove checks at start of script, vagrant only provisions once anyway
    - Remove unused packages used for headless browser testing
    - Update install steps for node and elasticsearch

--- Vagrantfile
+++ Vagrantfile
@@ -21,7 +21,9 @@ Vagrant.configure("2") do |config|
   end
 
   # Provision using the shell provisioner
-  config.vm.provision :shell, :path => "config/provision.sh"
+  config.vm.provision :shell,
+    :path => "config/provision.sh",
+    :privileged => false
 end
 
 # If you want to add additional configuration options then create a file
--- config/provision.sh
+++ config/provision.sh
@@ -2,55 +2,27 @@
 
 set -e
 
-# To prevent "dpkg-preconfigure: unable to re-open stdin: No such file or directory" warnings
-export DEBIAN_FRONTEND=noninteractive
-
-# Abort provisioning if toolchain is already installed
-command -v make chromium-browser npm mongo Xvfb chromedriver git >/dev/null 2>&1 &&
-{ echo "Everything already installed."; exit 0; }
-
-# Update package index before we start
-apt-get update -y
-
 # Instructions from: https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
-apt-get install -y python-software-properties
-add-apt-repository -y ppa:chris-lea/node.js
-apt-get update -y
-apt-get install -y python g++ make nodejs
+wget -qO - https://deb.nodesource.com/setup | sudo bash -
 
 # Instructions from: http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
-apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' > /etc/apt/sources.list.d/mongodb.list
-apt-get update -y
-apt-get install -y mongodb-org
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 
-# Install elasticsearch
-wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -
-echo 'deb http://packages.elasticsearch.org/elasticsearch/0.90/debian stable main' > /etc/apt/sources.list.d/elasticsearch.list
-apt-get update -y
-apt-get install -y openjdk-6-jre elasticsearch
+# Instructions from: http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/setup-repositories.html
+wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+echo 'deb http://packages.elasticsearch.org/elasticsearch/0.90/debian stable main' | sudo tee /etc/apt/sources.list.d/elasticsearch.list
 
-# Install other packages
-apt-get install -y ruby1.9.1 ruby1.9.1-dev chromium-browser xvfb git imagemagick graphicsmagick unzip postfix
+# Install packages
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+  nodejs build-essential \
+  mongodb-org \
+  openjdk-6-jre elasticsearch \
+  git imagemagick graphicsmagick postfix
 
-# Download and install chromedriver
-wget -q http://chromedriver.storage.googleapis.com/2.8/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-rm chromedriver_linux64.zip
-mv chromedriver /usr/local/bin
-chmod 755 /usr/local/bin/chromedriver
-
-# Install compass, watir-webdriver and pry
-gem install watir-webdriver --version=0.6.7 --no-rdoc --no-ri
-gem install pry --version=0.9.12.6 --no-rdoc --no-ri
-gem install sass --version=3.2.14 --no-rdoc --no-ri
-gem install compass --version=0.12.2 --no-rdoc --no-ri
-
-# Set up Xvfb to run all the time on display :99
-Xvfb :99 -ac &
-cp /etc/rc.local /etc/rc.local.old
-sed -i 's/^exit 0/Xvfb :99 -ac\n\nexit 0/' /etc/rc.local
-echo "export DISPLAY=:99" >> /home/vagrant/.bashrc
+sudo gem install sass --version=3.2.14 --no-rdoc --no-ri
+sudo gem install compass --version=0.12.2 --no-rdoc --no-ri
 
 echo "cd /vagrant" >> /home/vagrant/.bashrc
 cd /vagrant
