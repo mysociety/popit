b8ef5290ff45bf461a10ca0795c555279d2ec867
Zarino Zappia <mail@zarino.co.uk>
2015-03-02 17:29:51 +0000

    First iteration of new two-template source element layout.
    By separating the "Show source" button from the source "display" popup,
    we get more flexibilty over how the items are nested, which will come
    in useful for the crazy floating in the person/organization header.
    
    Separating the two elements requires the passing of some shared ID,
    so the JavaScript knows which buttons and popups to wire together.
    
    I wonder whether we could use the "field" attribute already passed
    to display-editing.html, instead of generating our own separate
    "sourceID" attribute?

--- public/js/augmenters/source-field.js
+++ public/js/augmenters/source-field.js
@@ -2,15 +2,14 @@ require(['jquery'], function($) {
   "use strict";
 
   var linkSelector = '.js-source__link';
-  var popupSelector = '.js-source__popup';
   var popupDisplayClass = 'source__popup--display';
 
   $(function() {
     $(document).on('click', linkSelector, function showSourceField(e) {
       e.preventDefault();
-      var $this = $(this);
-      var popup = $this.next(popupSelector);
-      popup.toggleClass(popupDisplayClass);
+      var displayID = $(this).attr('href');
+      var $display = $(displayID);
+      $display.toggleClass(popupDisplayClass);
     });
   });
 });
