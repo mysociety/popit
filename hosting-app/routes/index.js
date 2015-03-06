4121fce07640f70074acfdc0f57343fab1912008
Chris Mytton <chrismytton@gmail.com>
2015-03-06 11:05:33 +0000

    Add a JSON list of the authenticated users' instances
    This will allow openpoliticians to display a dropdown list of the
    users' instances so they can choose one to import their data into.

--- hosting-app/routes/index.js
+++ hosting-app/routes/index.js
@@ -4,6 +4,9 @@ var requireUser = require('../../lib/apps/auth').requireUser;
 var format = require('util').format;
 var config = require('config');
 var Error404 = require('../../lib/errors').Error404;
+var _ = require('underscore');
+var PopIt = require('../../lib/popit');
+var async = require('async');
 
 exports.route = function (app) {
 
@@ -55,27 +58,54 @@ exports.route = function (app) {
     res.redirect(format(config.instance_server.base_url_format, req.param('slug')));
   });
 
-  app.get('/instances', function(req, res, next){
-    res.locals.instances = [];
+  function findInstances(req, res, next) {
+    req.instances = res.locals.instances = [];
     req.popit.model('Instance').find(function(err, instances) {
       if (err) {
         return next(err);
       }
-      res.locals.all_instances = instances;
+      req.all_instances = res.locals.all_instances = instances;
       if (!req.user) {
-        return res.render('instances.html');
+        return next();
       }
 
       req.popit.permissions().find({ account: req.user.id }).populate('instance').exec(function(err, permissions) {
         if (err) {
           return next(err);
         }
-        res.locals.instances = permissions.map(function(permission) {
+        req.instances = res.locals.instances = permissions.map(function(permission) {
           return permission.instance;
         });
 
-        res.render('instances.html');
+        next();
       });
+    });
+  }
+
+  app.get('/instances', findInstances, function(req, res, next){
+    res.render('instances.html');
+  });
+
+  app.get('/instances.json', findInstances, function(req, res, next) {
+    if (!req.user) {
+      return res.status(401).jsonp({ errors: ["You are not authenticated"] });
+    }
+    async.map(req.instances, function(instance, done) {
+      var details = _.pick(instance, 'slug', 'created_date', 'base_url');
+      var popit = new PopIt();
+      popit.set_instance(instance.slug);
+      popit.load_settings(function(err) {
+        if (err) {
+          return done(err);
+        }
+        _.extend(details, popit._get_cached_settings());
+        done(null, details);
+      });
+    }, function(err, result) {
+      if (err) {
+        return next(err);
+      }
+      res.jsonp({ result: result });
     });
   });
 
